d7b309f2193041dbf1655d161629f560a4b175cb
marlonnbsb <marlonnbsb@gmail.com>
2020-06-17 13:22:16 -0300

    Add codepush

--- index.js
+++ index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import codePush from "react-native-code-push";
 
-AppRegistry.registerComponent(appName, () => App);
+let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
+MyApp = codePush(codePushOptions)(App);
+
+AppRegistry.registerComponent(appName, () => MyApp);
